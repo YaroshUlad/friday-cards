f452495105600d6b1f2858a45f59c9b4a97a8261
YaroshUlad <yarosh.uladzimir@gmail.com>
2022-08-23 19:22:04 +0300

    refactor app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,13 +3,11 @@ import React, { useEffect } from 'react';
 import './App.css';
 
 import { CircularProgress, LinearProgress } from '@mui/material';
-import { NavLink } from 'react-router-dom';
 
 import { me } from './app-reducer';
 import { useAppDispatch, useAppSelector } from './store';
 
 import { SimpleSnackbar } from 'common/components/simpleSnackbar/SimpleSnackbar';
-import { RoutePath } from 'common/enums/route-path';
 import Router from 'routes/Router';
 import { ReturnComponentType } from 'types/ReturnComponentType';
 
@@ -37,17 +35,6 @@ const App = (): ReturnComponentType => {
       )}
       <Router />
       <SimpleSnackbar />
-      <div style={{ height: '60px' }}>
-        <NavLink to={RoutePath.Error404}>404 </NavLink>-
-        <NavLink to={RoutePath.Profile}> Profile </NavLink>-
-        <NavLink to={RoutePath.Login}> Login </NavLink>-
-        <NavLink to={RoutePath.Register}> Register </NavLink>-
-        <NavLink to={RoutePath.ForgotPassword}> ForgotPassword </NavLink>-
-        <NavLink to={RoutePath.CheckEmail}> CheckEmail </NavLink>-
-        <NavLink to={RoutePath.CreateNewPassword}>CreateNewPassword </NavLink>-
-        <NavLink to={RoutePath.Packs}>Packs </NavLink>-
-        <NavLink to="modalTest">modal </NavLink>-
-      </div>
     </div>
   );
 };
